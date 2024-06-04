36c1172e97a345c96498603e228ce00448df1d02
3CNan <84979381+3CNan@users.noreply.github.com>
2024-06-04 19:17:08 +0800

    start music page

---
+++ music.js
@@ -0,0 +1,19 @@
+// film and tv section initialization
+function film_tv_init() {
+    // get elements from html
+    var section_bar_objs = document.getElementsByClassName("section_bar");
+
+    // initialize the amount of content bar needed
+    for (var i = 0; i < section_bar_objs.length; i++) {
+        section_init(section_bar_objs[i], all_film_tv[i], all_unaired[i], "film_tv");
+    }
+    section_load_image(all_film_tv, all_unaired, "film_tv");
+    class_write_video_source("section_bar", all_film_tv, all_unaired);
+    alert_init(all_film_tv);
+    class_write_video_source("alert_win", all_film_tv, [0, 0, 0]);
+}
+
+window.onload = function() {
+    carousel_init(masterpiece, "film_tv");
+    film_tv_init();
+}
